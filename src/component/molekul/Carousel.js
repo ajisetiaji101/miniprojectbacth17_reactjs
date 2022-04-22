8c3680a91f0b8d72e1e94a2cad69f1c496855eeb
AJI SETIAJI <ajisetiaji101@gmail.com>
2022-04-22 20:31:36 +0700

    update

--- src/component/molekul/Carousel.js
+++ src/component/molekul/Carousel.js
@@ -4,16 +4,13 @@ import Swipe from "react-easy-swipe";
 
 export const CarouselData = [
   {
-    image: "https://images.unsplash.com/photo-1546768292-fb12f6c92568?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1480&q=80",
+    image: "./assets/images/bootcamp.jpg",
   },
   {
-    image: "https://images.unsplash.com/photo-1501446529957-6226bd447c46?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1480&q=80",
+    image: "./assets/images/talent.jpg",
   },
   {
-    image: "https://images.unsplash.com/photo-1483729558449-99ef09a8c325?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1480&q=80",
-  },
-  {
-    image: "https://images.unsplash.com/photo-1475189778702-5ec9941484ae?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1480&q=80",
+    image: "./assets/images/hiring.jpg",
   },
 ];
 
@@ -52,14 +49,13 @@ class Carousel extends Component {
     return (
       <div className="overflow-hidden">
         <div className="w-screen h-screen flex overflow-hidden relative">
-          <AiOutlineLeft onClick={this.prevSlide} className="absolute left-4 text-3xl inset-y-1/2 text-white cursor-pointer" />
           {CarouselData.map((slide, index) => {
             return (
               <img
                 src={slide.image}
                 alt="This is a carousel slide"
                 key={index}
-                className={index === this.state.currentSlide ? "block w-full h-auto object-cover" : "hidden"}
+                className={index === this.state.currentSlide ? "block w-screen h-screen object-cover saturate-0" : "hidden"}
                 onMouseEnter={() => {
                   this.setState({ paused: true });
                 }}
@@ -69,6 +65,7 @@ class Carousel extends Component {
               />
             );
           })}
+          <AiOutlineLeft onClick={this.prevSlide} className="absolute left-4 text-3xl inset-y-1/2 text-white cursor-pointer" />
           <AiOutlineRight onClick={this.nextSlide} className="absolute right-4 text-3xl inset-y-1/2 text-white cursor-pointer" />
         </div>
       </div>
