33a6dd22af25edcb4d376955fc38129c48528f74
AJI SETIAJI <ajisetiaji101@gmail.com>
2022-04-23 00:08:16 +0700

    Penambahan Hero

--- src/component/molekul/Carousel.js
+++ src/component/molekul/Carousel.js
@@ -5,12 +5,15 @@ import Swipe from "react-easy-swipe";
 export const CarouselData = [
   {
     image: "./assets/images/bootcamp.jpg",
+    data: ["Join", "Bootcamp", "#"],
   },
   {
     image: "./assets/images/talent.jpg",
+    data: ["Need", "Talent ?", "#"],
   },
   {
     image: "./assets/images/hiring.jpg",
+    data: ["We", "Hiring", "#"],
   },
 ];
 
@@ -51,18 +54,35 @@ class Carousel extends Component {
         <div className="w-screen h-screen flex overflow-hidden relative">
           {CarouselData.map((slide, index) => {
             return (
-              <img
-                src={slide.image}
-                alt="This is a carousel slide"
-                key={index}
-                className={index === this.state.currentSlide ? "block w-screen h-screen object-cover saturate-0" : "hidden"}
-                onMouseEnter={() => {
-                  this.setState({ paused: true });
-                }}
-                onMouseLeave={() => {
-                  this.setState({ paused: false });
-                }}
-              />
+              <>
+                <img
+                  src={slide.image}
+                  alt="This is a carousel slide"
+                  key={index}
+                  className={index === this.state.currentSlide ? "block w-screen h-screen object-cover saturate-0 backdrop-opacity-50" : "hidden"}
+                  onMouseEnter={() => {
+                    this.setState({ paused: true });
+                  }}
+                  onMouseLeave={() => {
+                    this.setState({ paused: false });
+                  }}
+                />
+                <p key={index} className={index === this.state.currentSlide ? "absolute text-white left-1/4 top-1/4 text-4xl font-bold" : "hidden"}>
+                  {slide.data[0]}
+                </p>
+                <p key={index} className={index === this.state.currentSlide ? "absolute text-white left-1/4 top-1/3 text-6xl font-bold" : "hidden"}>
+                  {slide.data[1]}
+                </p>
+                <button
+                  className={
+                    index === this.state.currentSlide
+                      ? "absolute left-1/4 top-1/2  font-bold whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-md text-base font-medium text-white bg-gradient-to-r from-orange-600 to-red-600 hover:from-orange-700 hover:to-red-700"
+                      : "hidden"
+                  }
+                >
+                  Get Started
+                </button>
+              </>
             );
           })}
           <AiOutlineLeft onClick={this.prevSlide} className="absolute left-4 text-3xl inset-y-1/2 text-white cursor-pointer" />
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,18 @@
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     listStyleType: {
-      none: 'none',
-      disc: 'disc',
-      decimal: 'decimal',
-      square: 'square',
-      roman: 'upper-roman',
+      none: "none",
+      disc: "disc",
+      decimal: "decimal",
+      square: "square",
+      roman: "upper-roman",
     },
     extend: {
       animation: {
-        'spin-slow': 'spin 3s linear infinite',
-      }
+        "spin-slow": "spin 3s linear infinite",
+      },
     },
   },
-  plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/aspect-ratio'),
-    require('@tailwindcss/typography'),
-  ],
-}
+  plugins: [require("@tailwindcss/forms"), require("@tailwindcss/aspect-ratio"), require("@tailwindcss/typography")],
+};
