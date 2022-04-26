3ac040111326e56bcde38dcd8d0257256fad7470
AJI SETIAJI <ajisetiaji101@gmail.com>
2022-04-27 00:18:53 +0700

    add page placement

--- src/views/app/placement/Placement.js
+++ src/views/app/placement/Placement.js
@@ -1,7 +1,13 @@
-import React from 'react'
+import React from "react";
+import Page from "../../../component/commons/Page";
 
 export default function Placement() {
   return (
-    <div>Placement</div>
-  )
+    <Page title="Placement" titleButton="Create Placement">
+      <div className="text-center">
+        <span>Search by category </span>
+        <input className="border rounded-full px-4" placeholder="client, recruiter, phone number" />
+      </div>
+    </Page>
+  );
 }
