2ee86f362e0a0e63de7ef5be5db4445697facca5
fadhlur497 <fadhlurrahman497@gmail.com>
2022-05-16 10:18:49 +0700

    test

--- src/views/app/talent/Talent.js
+++ src/views/app/talent/Talent.js
@@ -215,8 +215,14 @@ export default function Talent() {
             </table>
             {listTalents.length === 0 && <div className="px-6 py-3 text-center whitespace-nowrap text-sm font-medium text-gray-900"> Data Not Found...</div>}
 
-            <div className="bg-white px-4 py-3 flex items-center justify-center border-t border-gray-200 sm:px-6">
-              <div className="hidden sm:flex-1 sm:flex sm:items-center sm:justify-center">
+            <div className="bg-white px-4 py-3 flex items-center justify-between border-t border-gray-200 sm:px-6">
+              <div className="hidden sm:flex-1 sm:flex sm:items-center sm:justify-between">
+                <div>
+                  <p className="text-sm text-gray-700">
+                    Showing <span className="font-medium">{(currentPage - 1) * 10 + 1}</span> to <span className="font-medium">{currentPage * 10 < listTalents.length ? currentPage * 10 : listTalents.length}</span> of{" "}
+                    <span className="font-medium">{listTalents.length}</span> results
+                  </p>
+                </div>
                 <div>
                   <nav className="relative z-0 inline-flex rounded-md shadow-sm -space-x-px" aria-label="Pagination">
                     <button
