b77ba553177aa2e422018c4b5b4595224d8e04fe
AJI SETIAJI <ajisetiaji101@gmail.com>
2022-05-19 22:45:04 +0700

    add baru

--- src/views/app/apply/Apply.js
+++ src/views/app/apply/Apply.js
@@ -1,8 +1,34 @@
+import { faAngleRight, faArrowRight, faHome } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import Page from "../../../component/commons/Page";
 
 export default function Apply() {
   const navigate = useNavigate();
-  return <Page title="Setting Profile" titleButton="Back" onClick={() => navigate(-1)}></Page>;
+  return (
+    <div className="container mx-auto px-10 bg-red-300">
+      <div className="py-4">
+        <FontAwesomeIcon icon={faHome} />
+        <span className=" font-medium"> Home</span>
+        <span>
+          {"   "}
+          <FontAwesomeIcon icon={faAngleRight} />
+        </span>
+        <span className=" font-medium"> Detail</span>
+      </div>
+      <h1 className="font-bold text-2xl text-center">Bootcamp Application Process</h1>
+      <div className="grid grid-cols-3 mx-auto bg-slate-700">
+        <div>
+          <h1>tes</h1>
+        </div>
+        <div>
+          <h1>tes2</h1>
+        </div>
+        <div>
+          <h1>tes3</h1>
+        </div>
+      </div>
+    </div>
+  );
 }
