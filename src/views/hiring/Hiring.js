6033330089f7ba21d7f7823c919e0426650cf7f0
fadhlur497 <fadhlurrahman497@gmail.com>
2022-05-19 18:22:01 +0700

    hiring update

--- src/views/hiring/Hiring.js
+++ src/views/hiring/Hiring.js
@@ -12,6 +12,7 @@ import { faClock } from '@fortawesome/free-solid-svg-icons';
 import { faCalendarCheck } from '@fortawesome/free-solid-svg-icons';
 import Carousel from './Carousel'
 import Sidebar from './Sidebar'
+import Search from './Search'
 
 
 export default function Hiring() {
@@ -45,10 +46,15 @@ export default function Hiring() {
 
   useEffect(() => {
     setListHiring(
-        Array.isArray(hiring) && hiring.filter(data=>(
-            (data.jobs_title.toLowerCase().includes(filter.input.toLowerCase())) 
-            ))
-        )
+        Array.isArray(hiring) && hiring.filter(data=> (
+          data.jobs_city.toLowerCase().includes(filter.input.toLowerCase()) 
+        // || 
+        // data.tale_bootcamp.toLowerCase().includes(filter.input.toLowerCase()
+        // )
+        ) 
+                  // &&
+          // (filter.select === 'Status' || data.tale_status_timeline.includes(filter.select))
+        ))
 }, [hiring]);
 
   // useEffect(() => {
@@ -57,33 +63,91 @@ export default function Hiring() {
   //   setPageRange(0);
   // }, [listHiring]);
 
-  // const handleOnChange = (name) => (event) => {
-  //   setFilter({ ...filter, [name]: event.target.value });
-  // };
+  const handleOnChange = (name) => (event) => {
+    setFilter({ ...filter, [name]: event.target.value });
+  };
 
-  // const onSearch = (event) => {
-  //   event.preventDefault();
-  //   setListHiring(
-  //     Array.isArray(hiring) &&
-  //       hiring.filter(
-  //         data =>(
-  //         (data.tale_fullname.toLowerCase().includes(filter.input.toLowerCase()) 
-  //         || 
-  //         data.tale_bootcamp.toLowerCase().includes(filter.input.toLowerCase()
-  //         )) &&
-  //         (filter.select === 'Status' || data.tale_status_timeline.includes(filter.select))
-  //         ))
-  //   )
-  // };
+  const onSearch = (event) => {
+    event.preventDefault();
+    setListHiring(
+      Array.isArray(hiring) &&
+        hiring.filter(
+          data =>(
+          (data.jobs_city.toLowerCase().includes(filter.input.toLowerCase()) 
+          // || 
+          // data.tale_bootcamp.toLowerCase().includes(filter.input.toLowerCase()
+          // )
+          ) 
+          // &&
+          // (filter.select === 'Status' || data.tale_status_timeline.includes(filter.select))
+          ))
+    )
+  };
 
   return (
   <div>
       <h1  class="text-xl font-serif" style={{marginLeft:"15px", fontWeight:'bold', marginTop:"15px"}}>Our Network</h1>
-
 <Carousel />
-<Sidebar />
+
+{/* search */}
+
+<div>
+                  <div className="w-full">
+            <div className="input-group relative flex justify-center items-stretch w-full mb-2">
+              <p className="text-sm mx-2 py-1">Search by Category</p>
+              <input
+                type="search"
+                // onChange={handleOnChange("input")}
+                className="form-control relative w-64 block px-4 py-0.5 ml-2 text-sm font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded-full transition ease-in-out m-0 focus:border-transparent focus:text-gray-700 focus:ring-1 focus:ring-offset-1 focus:ring-purple-500 focus:outline-none"
+                placeholder="Jabatan, Kata Kunci, Perusahaan"
+                aria-label="Search"
+                aria-describedby="button-addon2"
+              />
+                            <input
+                type="search"
+                onChange={handleOnChange("input")}
+                className="form-control relative w-64 block px-4 py-0.5 mr-2 ml-2 text-sm font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded-full transition ease-in-out m-0 focus:border-transparent focus:text-gray-700 focus:ring-1 focus:ring-offset-1 focus:ring-purple-500 focus:outline-none"
+                placeholder="Location"
+                aria-label="Search"
+                aria-describedby="button-addon2"
+              />
+              <select
+                name="tale_status_timeline"
+                id="tale_status_timeline"
+                // onChange={handleOnChange("select")}
+                className="capitalize form-select form-select-sm appearance-none block mx-1 px-2 py-0.5 w-24 text-sm font-normal text-gray-700 bg-white bg-clip-padding bg-no-repeat border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:border-transparent focus:text-gray-700 focus:outline-none focus:ring-1 focus:ring-offset-1 focus:ring-purple-500"
+                aria-label=".form-select-sm example"
+              >
+                <option>Status</option>
+                {/* {(tal_status || []).map((value, index) => 
+                (
+                  <option className="capitalize" value={value} key={index}>
+                    {value}
+                  </option>
+                )
+                )} */}
+              </select>
+              <button
+                type="submit"
+                onClick={onSearch}
+                className="btn px-3 py-1 bg-red-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-red-700 focus:outline-none focus:ring-1 focus:ring-offset-1 focus:ring-purple-500 transition duration-150 ease-in-out flex items-center"
+                id="button-addon2"
+              >
+                Search
+              </button>
+            </div>
+          </div>
+          <br></br>
+
+    </div>
+
+
 {/* Card */}
-<div className="grid grid-cols-2 justify-items-end ">
+<div className="flex">
+  <div>
+  <Sidebar />
+</div>
+<div className="grid grid-cols-2 ml-80 gap-4 sm:grid-cols-2 ">
   
 {Array.isArray(listHiring)&&listHiring.map((data) => (
                     
@@ -107,7 +171,7 @@ export default function Hiring() {
 ))}
 
             </div>
-
+</div>
 
 
 </div>
