3d73618c62dc0a90233b49b0b899d132e68cfc59
fadhlur497 <fadhlurrahman497@gmail.com>
2022-05-21 22:46:21 +0700

    masih hiring

--- src/views/hiring/Hiring.js
+++ src/views/hiring/Hiring.js
@@ -18,6 +18,7 @@ import Search from './Search'
 
 
 const Har_status =['It Programmer','Marketing','Sales']
+const pek_status =['Magang','Full-time','Part-time','Freelance']
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
 }
@@ -59,14 +60,27 @@ export default function Hiring() {
         Array.isArray(hiring) && hiring.filter(data=> (
         data.jobs_city.toLowerCase().includes(filter.input.toLowerCase()) || 
         data.jobs_title.toLowerCase().includes(filter.input.toLowerCase())) &&
-        (filter.select === 'All' || data.jobs_specification.includes(filter.select))
+        (filter.select === 'All' || data.jobs_specification.includes(filter.select))&&
+        (data.jobs_working_type.toLowerCase().includes(filter.input.toLowerCase())
+        )
         // (filter.checkbox || data.jobs_working_type.includes(filter.checkbox))
         ))
 }, [hiring]);
 
+// useEffect(() => {
+//   setListHiring(
+//       Array.isArray(hiring) && hiring.filter(data=> 
+//       (data.jobs_working_type.toLowerCase().includes(filter.input.toLowerCase()))
+//       ))
+//       onSearch( )
+// }, [hiring]);
+
+
+
   useEffect(() => {
     setPageNumbers(Array.from({ length: Math.ceil(listHiring.length / 4) }, (v, i) => (i + 1 === 1 ? { number: i + 1, active: true } : { number: i + 1, active: false })));
     setCurrentPage(1);
+    console.log (hiring)
     setPageRange(0);
   }, [listHiring]);
 
@@ -82,38 +96,25 @@ export default function Hiring() {
           data =>(
           (data.jobs_city.toLowerCase().includes(filter.input.toLowerCase()) || 
           data.jobs_title.toLowerCase().includes(filter.input.toLowerCase())) &&
+          // data.jobs_working_type.toLowerCase().includes(filter.input.toLowerCase())&&
           (filter.select === 'All' || data.jobs_specification.includes(filter.select)) 
           ))
     )
   };
 
-//   const [typeFilter, setTypeFilter] = useState("")
-//   const [dataMember, setDataMember] = useState("")
+  const onCombo = (name) => (event) => {
+    event.preventDefault();
+    setFilter({ ...filter, [name]: event.target.value });
+    setListHiring(
+      Array.isArray(hiring) &&
+        hiring.filter(
+          data =>(
+            // (filter.input === 'All' || data.jobs_working_type.includes(filter.input)) 
+          (data.jobs_working_type.toLowerCase().includes(filter.input.toLowerCase())
+	)
+	)))
+	}
 
-//   useEffect(()=>{
-//     const filterCheck = hiring.results.filter(
-//       (member) => member.tipe === typeFilter
-//     );
-//     setListHiring(filterCheck)
-//   }, [typeFilter])
-
-//   useEffect(() => {
-//     setListHiring(
-//         Array.isArray(hiring) && hiring.filter(data=> (
-//         data.jobs_city.toLowerCase().includes(filter.input.toLowerCase()) || 
-//         data.jobs_title.toLowerCase().includes(filter.input.toLowerCase())) &&
-//         (filter.select === 'All' || data.jobs_specification.includes(filter.select))
-//         // (filter.checkbox || data.jobs_working_type.includes(filter.checkbox))
-//         ))
-// }, [hiring]);
-//   const checkboxFilterValue = {
-//     a : "Magang",
-//     b : "full-time",
-//     c : "part-time",
-//     d : "freelance"
-//   }
-
-//   console.log(listHiring)
   return (
   <div>
       <h1  class="text-xl font-serif" style={{marginLeft:"15px", fontWeight:'bold', marginTop:"15px"}}>Our Network</h1>
@@ -175,6 +176,152 @@ export default function Hiring() {
 <div className="flex">
 
 {/* sidebar */}
+
+<aside className="w-72  h-full shadow-md bg-white absolute" aria-label="Sidebar">
+    <div className="overflow-y-auto py-4 px-3 bg-gray-50   rounded dark:bg-gray-800">
+        <ul className="space-y-2">
+          <li>
+              <a href="#" className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
+              <svg className="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z"></path><path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z"></path></svg>
+      <span className="ml-3">Filter Pencarianmu</span>
+              </a>
+          </li>
+      <li>
+        <button type="button" className="flex items-center p-2 w-full text-base font-normal text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700" aria-controls="dropdown-example" data-collapse-toggle="dropdown-example">
+          <svg className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 2a4 4 0 00-4 4v1H5a1 1 0 00-.994.89l-1 9A1 1 0 004 18h12a1 1 0 00.994-1.11l-1-9A1 1 0 0015 7h-1V6a4 4 0 00-4-4zm2 5V6a2 2 0 10-4 0v1h4zm-6 3a1 1 0 112 0 1 1 0 01-2 0zm7-1a1 1 0 100 2 1 1 0 000-2z" clip-rule="evenodd"></path></svg>
+          <span className="flex-1 ml-3 text-left whitespace-nowrap" sidebar-toggle-item="">Tampilkan Berdasarkan</span>
+          <svg sidebar-toggle-item="" className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+        </button>
+      <ul id="dropdown-example" className="hidden py-2 space-y-2">
+      <li className="relative">
+          <div className="flex space-x-2 justify-center">
+            <button type="button" className="inline-block px-4 py-1.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Match</button>
+            <button type="button" className="inline-block px-4 py-1.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Newest</button>
+          </div>
+        </li>
+      </ul>
+      </li>
+      <li>
+      <button type="button" className="flex items-center p-2 w-full text-base font-normal text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700" aria-controls="dropdown-example" data-collapse-toggle="dropdown-example1">
+          <svg className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 2a4 4 0 00-4 4v1H5a1 1 0 00-.994.89l-1 9A1 1 0 004 18h12a1 1 0 00.994-1.11l-1-9A1 1 0 0015 7h-1V6a4 4 0 00-4-4zm2 5V6a2 2 0 10-4 0v1h4zm-6 3a1 1 0 112 0 1 1 0 01-2 0zm7-1a1 1 0 100 2 1 1 0 000-2z" clip-rule="evenodd"></path></svg>
+      <span className="flex-1 ml-3 text-left whitespace-nowrap" sidebar-toggle-item="">Tipe Pekerjaan</span>
+          <svg sidebar-toggle-item="" className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+      </button>
+      <ul id="dropdown-example1" className="hidden py-2 space-y-2"> 
+      <li className="relative ml-12">
+      {(pek_status || []).map((value, index) => 
+                (
+        <div className="form-check"> 
+      <input 
+                      type="checkbox"
+                      onChange={onCombo("input")} 
+                      value={value}
+                      // onClick={onSearch()}
+                      className="form-check-input appearance-none h-3.5 w-3.5 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" id="flexCheckDefault"/>
+      <label className="form-check-label inline-block text-gray-800" for="flexCheckDefault" style={{fontSize:"12.5px"}}>
+                  <option className="capitalize" value={value} key={index}>
+                    {value}
+                  </option>
+
+      </label>
+    </div>
+                )
+                )}
+        </li>
+        </ul>
+        
+      </li>
+      <li>
+      <button type="button" className="flex items-center p-2 w-full text-base font-normal text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700" aria-controls="dropdown-example" data-collapse-toggle="dropdown-example3">
+          <svg className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 2a4 4 0 00-4 4v1H5a1 1 0 00-.994.89l-1 9A1 1 0 004 18h12a1 1 0 00.994-1.11l-1-9A1 1 0 0015 7h-1V6a4 4 0 00-4-4zm2 5V6a2 2 0 10-4 0v1h4zm-6 3a1 1 0 112 0 1 1 0 01-2 0zm7-1a1 1 0 100 2 1 1 0 000-2z" clip-rule="evenodd"></path></svg>
+      <span className="flex-1 ml-3 text-left whitespace-nowrap" sidebar-toggle-item="">Pengalaman</span>
+          <svg sidebar-toggle-item="" className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+      </button>
+      <ul id="dropdown-example3" className="hidden py-2 space-y-2">
+      <li className="relative ml-12">
+      <div className="form-check">
+      <input className="form-check-input appearance-none h-3.5 w-3.5 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="checkbox" value="" id="flexCheckDefault"/>
+      <label className="form-check-label inline-block text-gray-800" for="flexCheckDefault" style={{fontSize:"12.5px"}}>
+        Kurang 1Tahun
+      </label>
+    </div>
+    <div className="form-check">
+      <input className="form-check-input appearance-none h-3.5 w-3.5 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="checkbox" value="" id="flexCheckDefault"/>
+      <label className="form-check-label inline-block text-gray-800" for="flexCheckDefault" style={{fontSize:"12.5px"}}>
+        1-3Tahun
+      </label>
+    </div>
+    <div className="form-check">
+      <input className="form-check-input appearance-none h-3.5 w-3.5 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="checkbox" value="" id="flexCheckDefault"/>
+      <label className="form-check-label inline-block text-gray-800" for="flexCheckDefault" style={{fontSize:"12.5px"}}>
+        5-10Tahun
+      </label>
+    </div>
+    <div className="form-check">
+      <input className="form-check-input appearance-none h-3.5 w-3.5 border border-gray-200 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="checkbox" value="" id="flexCheckDefault"/>
+      <label className="form-check-label inline-block text-gray-800" for="flexCheckDefault" style={{fontSize:"12.5px"}}>
+        Kurang 10Tahun
+      </label>
+    </div>
+        </li>
+        </ul>
+      </li>
+
+      <li>
+      <button type="button" className="flex items-center p-2 w-full text-base font-normal text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700" aria-controls="dropdown-example" data-collapse-toggle="dropdown-example4">
+          <svg className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 2a4 4 0 00-4 4v1H5a1 1 0 00-.994.89l-1 9A1 1 0 004 18h12a1 1 0 00.994-1.11l-1-9A1 1 0 0015 7h-1V6a4 4 0 00-4-4zm2 5V6a2 2 0 10-4 0v1h4zm-6 3a1 1 0 112 0 1 1 0 01-2 0zm7-1a1 1 0 100 2 1 1 0 000-2z" clip-rule="evenodd"></path></svg>
+      <span className="flex-1 ml-3 text-left whitespace-nowrap" sidebar-toggle-item="">Remotely</span>
+          <svg sidebar-toggle-item="" className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+      </button>
+      <ul id="dropdown-example4" className="hidden py-2 space-y-2">
+      <li className="relative ml-12">
+      <label for="default-toggle" className="inline-flex relative items-center cursor-pointer ml-10">
+  <input type="checkbox" value="" id="default-toggle" className="sr-only peer"/>
+  <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+</label>
+        </li>
+        </ul>
+      </li>
+      <li>
+      <button type="button" className="flex items-center p-2 w-full text-base font-normal text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700" aria-controls="dropdown-example" data-collapse-toggle="dropdown-example5">
+          <svg className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 2a4 4 0 00-4 4v1H5a1 1 0 00-.994.89l-1 9A1 1 0 004 18h12a1 1 0 00.994-1.11l-1-9A1 1 0 0015 7h-1V6a4 4 0 00-4-4zm2 5V6a2 2 0 10-4 0v1h4zm-6 3a1 1 0 112 0 1 1 0 01-2 0zm7-1a1 1 0 100 2 1 1 0 000-2z" clip-rule="evenodd"></path></svg>
+      <span className="flex-1 ml-3 text-left whitespace-nowrap" sidebar-toggle-item="">Terupdate</span>
+          <svg sidebar-toggle-item="" className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+      </button>
+      <ul id="dropdown-example5" className="hidden py-2 space-y-2">
+      <li className="relative ml-12">
+      <div className="form-check">
+      <input className="form-check-input appearance-none h-3.5 w-3.5 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="checkbox" value="" id="flexCheckDefault"/>
+      <label className="form-check-label inline-block text-gray-800" for="flexCheckDefault" style={{fontSize:"12.5px"}}>
+        24Jam Terakhir
+      </label>
+    </div>
+    <div className="form-check">
+      <input className="form-check-input appearance-none h-3.5 w-3.5 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="checkbox" value="" id="flexCheckDefault"/>
+      <label className="form-check-label inline-block text-gray-800" for="flexCheckDefault" style={{fontSize:"12.5px"}}>
+        Seminggu Terakhir
+      </label>
+    </div>
+    <div className="form-check">
+      <input className="form-check-input appearance-none h-3.5 w-3.5 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="checkbox" value="" id="flexCheckDefault"/>
+      <label className="form-check-label inline-block text-gray-800" for="flexCheckDefault" style={{fontSize:"12.5px"}}>
+        Sebelum Terakhir
+      </label>
+    </div>
+    <div className="form-check">
+      <input className="form-check-input appearance-none h-3.5 w-3.5 border border-gray-200 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="checkbox" value="" id="flexCheckDefault"/>
+      <label className="form-check-label inline-block text-gray-800" for="flexCheckDefault" style={{fontSize:"12.5px"}}>
+        kapanpun
+      </label>
+    </div>
+        </li>
+        </ul>
+      </li>
+
+
+        </ul>
+        </div>
+      </aside>
 {/* <div>
       <aside className="w-72  h-full shadow-md bg-white absolute" aria-label="Sidebar">
     <div className="overflow-y-auto py-4 px-3 bg-gray-50   rounded dark:bg-gray-800">
@@ -224,11 +371,13 @@ export default function Hiring() {
     </div> */}
 
 {/* card */}
+
 <div className="grid grid-cols-2 ml-80 gap-4 sm:grid-cols-2 bg-slate-50">
   
 {Array.isArray(listHiring)&&listHiring.slice((currentPage-1)*4,currentPage*4).map((data) => (
                     
         <div className=" flex justify-end col-2 ml-10 mr-10  " >
+          
   <div className="flex flex-wrap bg-white shadow-lg mt-10 ">
   <div className="grid grid-cols-2">
   <div className="ml-3 "><img className="w-20 mb-5 h-10 w-10 rounded-full" src={`${config.domain}/hiring/images/${data.jobs_photo}`} alt={`${data.jobs_id}`} style={{width:"150px", height:"150px"}}/></div>
