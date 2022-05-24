307afd4756f4ac3837d4606d415b83659713ef46
Azian <azianas012@gmail.com>
2022-05-24 10:38:58 +0800

    fix nav link

--- src/views/app/curriculum/EditCurriculum.js
+++ src/views/app/curriculum/EditCurriculum.js
@@ -527,7 +527,7 @@ export default function EditCurriculum({ match }) {
     <Page
       title="Edit Curriculum"
       titleButton="Back"
-      onClick={() => navigate(-1)}
+      onClick={() => navigate("/app/curriculum")}
     >
       <Formik
         initialValues={curriculumValues}
@@ -825,6 +825,7 @@ export default function EditCurriculum({ match }) {
                 </button>
                 <button
                   type="button"
+                  onClick={() => navigate("/app/curriculum")}
                   className="inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-gray-500 hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500 sm:order-1 sm:ml-3"
                 >
                   Cancel
