257cbec757ae249a070fe45428b503ee4faf58bc
AJI SETIAJI <ajisetiaji101@gmail.com>
2022-05-27 22:25:35 +0700

    fix foto

--- src/views/app/setting/Setting.js
+++ src/views/app/setting/Setting.js
@@ -191,7 +191,7 @@ export default function Setting() {
         };
         dispatch(doUpdateTalentNoFileRequest(payload));
       }
-      // navigate("/", { state: { refresh: true } });
+      navigate("/", { state: { refresh: true } });
     },
   });
 
